c1fbe2ef8db8a7aea3cd9dfcc011564071d575ba
AzeemX-Dev <wajeehazeem56@gmail.com>
2022-11-20 18:54:08 +0500

    R_CLONE_COMMIT1

---
+++ includes/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+    // your database's name
+    define("DATABASE", "reddit_clone");
+
+    // your database's username
+    define("USERNAME", "root");
+
+    // your database's password
+    define("PASSWORD", "NewPassword");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+?>
---
+++ php/new_soc.php
@@ -0,0 +1,13 @@
+<?php
+
+	require("../includes/config.php");
+
+	if (!isset($_POST["soc"]))
+		redirect("home.php");
+
+	if(make_soc($_POST["soc"]) === false)
+		apologize("Failed to create society: ".$_POST["soc"]);
+
+	redirect("soc.php?soc=".$_POST["soc"]);
+
+?>
