a35f06eaf8b2406974420a4f575bdf72fcb098f6
ShafayKashif <109824354+ShafayKashif@users.noreply.github.com>
2022-12-09 22:10:54 +0500

    Update usocs.php

--- templates/usocs.php
+++ templates/usocs.php
@@ -2,19 +2,19 @@
   $(document).ready(function () {
     $('#umsocs').DataTable();
     $('#ussocs').DataTable();
-    $('#umsocs_wrapper').css({"padding":"10px"});
-    $('#ussocs_wrapper').css({"padding":"10px"});
+    $('#umsocs_wrapper').css({"padding":"75px"});
+    $('#ussocs_wrapper').css({"padding":"75px"});
   });
 </script>
 
 <?php
 
-	$table = div(div(par("Societies you moderate"), "panel-heading"), "panel panel-primary");
+	$table = div(div(par("Subreddits you moderate"), "panel-heading"), "panel panel-primary");
 	$table["children"][] = make_table($msubs, ["society", "mod since"], "table", "umsocs", [], [0]);
 
 	echo to_html($table);
 
-	$table = div(div(par("Subscribed societies"), "panel-heading"), "panel panel-primary");
+	$table = div(div(par("Subscribed Subreddits"), "panel-heading"), "panel panel-primary");
 	$table["children"][] = make_table($subs, ["society", "subbed since"], "table", "ussocs", [], [0]);
 
 	echo to_html($table);
